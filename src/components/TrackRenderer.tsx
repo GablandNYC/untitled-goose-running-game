138ec78c3dba249deaaee246c6a1ee004f8baac0
Maxime Heckel <heckelmaxime@gmail.com>
2026-03-28 17:25:11 -0400

    Hide track curve

--- src/components/TrackRenderer.tsx
+++ src/components/TrackRenderer.tsx
@@ -1,12 +1,12 @@
-import { useMemo } from "react";
-import { trackCurve } from "@/core/track";
+// import { useMemo } from "react";
+// import { trackCurve } from "@/core/track";
 
 export function TrackRenderer() {
-  const points = useMemo(() => trackCurve.getPoints(100), []);
+  // const points = useMemo(() => trackCurve.getPoints(100), []);
 
   return (
     <group>
-      <line>
+      {/* <line>
         <bufferGeometry>
           <bufferAttribute
             args={[new Float32Array(points.flatMap((p) => [p.x, p.y, p.z])), 3]}
@@ -15,10 +15,15 @@ export function TrackRenderer() {
             itemSize={3}
           />
         </bufferGeometry>
-        <lineBasicMaterial color="#888" linewidth={2} />
-      </line>
+        <lineBasicMaterial
+          color="#888"
+          linewidth={2}
+          transparent={true}
+          opacity={0.0}
+        />
+      </line> */}
       <mesh rotation-x={-Math.PI / 2} position-y={-0.01}>
-        <planeGeometry args={[100, 50]} />
+        <planeGeometry args={[200, 50]} />
         <meshStandardMaterial color="#2d5a27" />
       </mesh>
     </group>
